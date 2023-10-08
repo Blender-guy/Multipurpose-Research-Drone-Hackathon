2c51a8464ead9db69f66544fda49a01fcc038f32
Blender-guy <147308242+Blender-guy@users.noreply.github.com>
2023-10-08 05:11:02 -0400

    Add files via upload

---
+++ hackathon3.ino
@@ -0,0 +1,57 @@
+/*
+* Arduino Wireless Communication Tutorial
+*       Example 1 - Receiver Code
+*                
+* by Dejan Nedelkovski, www.HowToMechatronics.com
+* 
+* Library: TMRh20/RF24, https://github.com/tmrh20/RF24/
+*/
+
+#include <SPI.h>
+#include <nRF24L01.h>
+#include <RF24.h>
+#include <Servo.h>
+
+Servo s1;
+Servo s2;
+Servo s3;
+Servo s4;
+Servo s5;
+
+RF24 radio(7, 8); // CE, CSN
+
+const byte address[6] = "00001";
+const int num = 4;
+int array[num];
+int newArray[num];
+int stickLeftX = 0;
+int stickLeftY = 0;
+int stickRightX = 0;
+int stickRightY = 0;
+
+void setup() {
+  s.attach(2);
+  pinMode(7, OUTPUT);
+  pinMode(8, OUTPUT);
+  Serial.begin(9600);
+  radio.begin();
+  radio.openReadingPipe(0, address);
+  radio.setPALevel(RF24_PA_MIN);
+  radio.startListening();
+}
+
+void loop() {
+  if (radio.available()){ 
+    radio.read( &array, sizeof(array) ); 
+    Serial.println(array[0]);
+    stickLeftX = map(array[0], 0, 1023, 0, 180);
+    stickLeftY = map(array[1], 0, 1023, 0, 180);
+    stickRightX = map(array[2], 0, 1023, 0, 180);
+    stickRightY = map(array[3], 0, 1023, 0, 180);
+    Serial.println(stickLeftX);
+   }
+   //Serial.println(stickLeftX);
+  s.write(stickLeftX);
+  s.write(stickLeftY);
+  delay(100);
+}
